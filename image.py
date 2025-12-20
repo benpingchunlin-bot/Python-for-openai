83d38983e0973c24317b7fa0a206e37ce78fce78
Pingchun Lin <ben.pingchun.lin@gmail.com>
2025-12-20 09:44:05 +0800

    Add image

---
+++ image.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+
+url = "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"
+response = Client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "Give the name of the animal in the image."},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": url
+                    },
+                },
+            ],
+        }
+    ],
+)
+print(response.choices[0].message.content)
